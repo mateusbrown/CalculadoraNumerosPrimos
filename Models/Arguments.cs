e5553b7bb20540a0f71436e5ede3046d4ec66fea
Mateus Reinaldo <mateusbrown@hotmail.com>
2025-10-29 21:58:56 +0000

    Ajustes

--- Models/Arguments.cs
+++ Models/Arguments.cs
@@ -1,1 +1,9 @@
-
+namespace CalculadoraNumerosPrimos.Models
+{
+    public class Arguments
+    {
+        public int InitialValue { get; set; } = int.MinValue;
+        public int FinalValue { get; set; } = int.MaxValue;
+        public int ThreadCount { get; set; } = 4;
+    }
+}
